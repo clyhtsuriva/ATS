06ad4b73d54beb6f81d0b43d49f2a0701c87fcfb
Clyhtsuriva Synck <clyhtsuriva>
2020-03-01 14:18:05 +0100

    web update

---
+++ web/fonctions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def baseHTML(title,body):
+    content=("""<!DOCTYPE html>
+<html>
+        <head>
+                <title>"""+ title +"""</title>
+                <meta charset="UTF-8">
+                <link rel="stylesheet" type="text/css" href="styde.css">
+        </head>
+        <body>"""+ body +"""</body>
+</html>
+
+            """)
+    return content
---
+++ web/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import mod_python
+import fonctions
+
+def index(req):
+    req.content_type="text/html"
+    req.write(fonctions.baseHTML("Test","This is a test page."))
