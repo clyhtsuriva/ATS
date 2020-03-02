a43dc49ffbdb4abccd775766fc20dd53f76f6f28
Masfehico <clyhtsuriva@gmail.com>
2020-03-02 12:58:16 +0100

    script modified

--- analyse/script.sh
+++ analyse/script.sh
@@ -3,16 +3,16 @@
 while :
 do
 
-	sudo tcpdump -i eth1 -nn -c1 -A src 192.168.52.19 -w capture.pcap 
-	sudo tcpdump -r capture.pcap > grostas 
-	cat grostas | cut -d" " -f1 >> heure.txt
-	cat grostas | cut -d" " -f2 >> protocole.txt
-	cat grostas | cut -d" " -f3 >> source.txt
-	cat grostas | cut -d" " -f5 >> destination.txt	
-	tail -n1 heure.txt
-	tail -n1 protocole.txt
-	tail -n1 source.txt
-	tail -n1 destination.txt
+	sudo tcpdump -i eth1 -nn -c1 -A src 192.168.52.19 -w capture.pcap
+	sudo tcpdump -r capture.pcap > grostas
+	cat grostas | cut -d" " -f1 >> /tmp/heure.txt
+	cat grostas | cut -d" " -f2 >> /tmp/protocole.txt
+	cat grostas | cut -d" " -f3 >> /tmp/source.txt
+	cat grostas | cut -d" " -f5 >> /tmp/destination.txt
+	tail -n1 /tmp/heure.txt
+	tail -n1 /tmp/protocole.txt
+	tail -n1 /tmp/source.txt
+	tail -n1 /tmp/destination.txt
 done
 
 
