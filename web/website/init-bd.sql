85534f576e13cbb05d48f2c8210c917a38085c3a
root <clyhtsuriva@gmail.com>
2020-03-05 13:02:26 +0100

    sql comment

--- web/website/init-bd.sql
+++ web/website/init-bd.sql
@@ -1,30 +1,4 @@
---Example used in a previous project
-
-/*
-drop table if exists util cascade;
-drop table if exists contact cascade;
-
-create table util(
-	id_util serial primary key,
-	login varchar(20) not null,
-	mdp varchar(20)
-);
-
-create table contact(
-	id_contact serial primary key,
-	nom varchar(100),
-	email varchar(100),
-	tel varchar(10),
-	adresse varchar(100),
-	latitude real,
-	longitude real,
-	id_util smallint not null references util
-);
-
-insert into util(login,mdp) values ('root','root');
-insert into util(login,mdp) values ('pico','pico');
-*/
-
+--Creation of the main table
 drop table if exists trame cascade;
 
 create table trame(
@@ -34,3 +8,5 @@ create table trame(
 	source varchar(100),
 	destination varchar(100)
 );
+
+
