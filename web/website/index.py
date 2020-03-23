86cdb9caa48d7a7069978dd127be61169b7c7e95
cly <clyhtsuriva@gmail.com>
2020-03-23 13:56:38 +0100

    index.py modified

--- web/website/index.py
+++ web/website/index.py
@@ -2,10 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import mod_python
-import fonctions
+from fonctions import baseHTML, connexionBD
 
 def index(req):
     req.content_type="text/html"
-    req.write(fonctions.baseHTML("ATS-Project","""
-<center><h1>ATS-Project</h1></center>
-"""))
+    
+    content=str()
+
+#sql part    
+    conn=connexionBD()
+    cur=conn.cursor()
+    sql="select * from trame;"
+    cur.execute(sql)
+    conn.commit()
+    data=cur.fetchall()
+    conn.close()
+#sql part
+
+    for i in data :
+        content+=("""<ul><li>""" + str(i[1]) + str(i[2]) + str(i[3]) + str(i[4]) + """</li></ul>""")
+    
+    req.write(baseHTML("ATS-Project","""
+<center><h1>ATS-Project</h1></center>"""
++ content))
+
