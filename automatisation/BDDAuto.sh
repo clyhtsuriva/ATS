2ec68e33b8ee974541df1be15b8d82c37e284da4
clyhtsuriva <clyhtsuriva@gmail.com>
2020-05-08 07:11:18 +0200

    debug et commentaires

--- automatisation/BDDAuto.sh
+++ automatisation/BDDAuto.sh
@@ -13,9 +13,10 @@ echo atsuser:123456 | chpasswd
 
 echo "CREATE USER atsuser WITH PASSWORD '123456'; CREATE DATABASE atsdb; GRANT ALL PRIVILEGES ON DATABASE atsdb to atsuser;" > /tmp/conf
 
+service postgresql start #dernier ajout : Lancement du serveur
 
-su - postgres -c "psql -f /tmp/conf"
+su - postgres -c "psql -f /tmp/conf" #creation de l'util atsuser et la bdd atsdb sur le serv psql
 
 
-pushd /tmp ; su atsuser -c "psql -d atsdb -U atsuser -f /tmp/table.sql" ; popd
+pushd /tmp ; su atsuser -c "psql -d atsdb -U atsuser -f /tmp/table.sql" ; popd #mis en place de la table paquet dans la bdd
 
